4a1258f420545ba409e6456346784306a43a72a2
Andrey Nikulin <nikulinandrey888@gmail.com>
2025-12-08 10:16:41 +0300

    feat: add login page and protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,37 @@
+import React from "react";
+import { useAuth } from "./hooks/useAuth";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+import { LoginPage } from "./pages/login-page/LoginPage";
+import HomePage from "./pages/home-page/HomePage";
+
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!import.meta.env.VITE_ENABLE_AUTH) return <>{children}</>;
+
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
+};
+
 function App() {
   return (
-    <>
-      <div>ToDo LIST</div>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={<ProtectedRoute>{<HomePage />}</ProtectedRoute>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
